8c1ebd83f01f2738564e4a04e56bc64070254361
Nick Frost <nfrost.dev@gmail.com>
2021-02-01 15:40:01 -0500

    pwa configuration

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    pwa: {
+        name: 'Groceries List',
+        short_name: 'Groceries List',
+        themeColor: '#111827',
+        msTileColor: '#111827',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+        workboxPluginMode: 'InjectManifest',
+        workboxOptions: {
+            swSrc: 'src/registerServiceWorker.js',
+        }
+    }
+}
