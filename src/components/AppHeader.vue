49bd0928f65a8596ce88150172544455ebccd73d
Nick Frost <nfrost.dev@gmail.com>
2021-02-01 11:39:40 -0500

    favicon update

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,25 @@
 <template>
   <header class="header">
-    <router-link to="'/" title="View my Lists" class="header__link">
-      <font-awesome-icon icon="list-alt" class="header__link__icon"/>
-      <span>My Lists</span>
-    </router-link>
-    <router-link to="/new" title="Create a new List" class="header__link header__link--new-list">
-      <font-awesome-icon icon="plus-circle" class="header__link__icon"/>
-      <span>New List</span>
-    </router-link>
+    <nav aria-label="Primary" class="header__nav">
+      <router-link to="'/" title="View my Lists" class="header__link">
+        <font-awesome-icon icon="list-alt" class="header__link__icon"/>
+        <span>My Lists</span>
+      </router-link>
+      <router-link to="/new" title="Create a new List" class="header__link header__link--new-list">
+        <font-awesome-icon icon="plus-circle" class="header__link__icon"/>
+        <span>New List</span>
+      </router-link>
+    </nav>
   </header>
 </template>
 
 <style lang="scss">
 .header {
   @apply flex justify-center items-center p-6 text-xl bg-gray-800 border-b border-gray-600;
+
+  &__nav {
+    @apply flex justify-between items-center w-full max-w-lg;
+  }
 
   &__link {
     @apply flex justify-center items-center;
