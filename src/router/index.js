39b076f263a44f34bc13803f7e9bc4ded6f8ba5c
Nick Frost <nickfrost.us@gmail.com>
2021-09-04 11:24:38 -0400

    pwa, route, and store migrations

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,14 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/new',
+    name: 'New List',
+    component: () => import(/* webpackChunkName: "new" */ '../views/New.vue')
+  },
+  {
+    path: '/:id',
+    name: 'List',
+    component: () => import(/* webpackChunkName: "list" */ '../views/List.vue')
   }
 ]
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,53 @@
 import { createStore } from 'vuex'
+import { v4 as uuidv4 } from 'uuid'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    lists: []
+  },
+  mutations: {
+    initListFromLocalStorage (state) {
+      if (localStorage.getItem('lists')) {
+        state.lists = JSON.parse(localStorage.getItem('lists'))
+      }
+    },
+    mutateList (state, list) {
+      if (list.id) {
+        const existingList = state.lists.find(existingList => existingList.id === list.id)
+        // Check if the list is linked from another person and being updated/overridden.
+        if (existingList) {
+          state.lists[state.lists.findIndex(existingList => existingList.id === list.id)] = list
+        } else {
+          state.lists.push(list)
+        }
+      } else {
+        list.id = uuidv4()
+        state.lists.push(list)
+      }
+      localStorage.setItem('lists', JSON.stringify(state.lists))
+    },
+    removeList (state, id) {
+      state.lists.splice(state.lists.findIndex(list => list.id === id), 1)
+      localStorage.setItem('lists', JSON.stringify(state.lists))
+    }
+  },
+  actions: {
+    updateList (context, list) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          context.commit('mutateList', list)
+          resolve()
+        }, 1)
+      })
+    },
+    deleteList (context, id) {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          context.commit('removeList', id)
+          resolve()
+        }, 1)
+      })
+    }
+  },
   modules: {}
 })
