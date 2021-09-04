d1c0a90c1040ea8f496ef72ae8a6b10c15be8fab
Nick Frost <nickfrost.us@gmail.com>
2021-09-04 12:14:55 -0400

    view updates, started on new

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Lists from '../views/Lists.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Lists',
+    component: Lists
   },
   {
     path: '/new',
---
+++ src/views/New.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="new-list">
+    <form class="new-list__form">
+
+      <div class="new-list__container">
+        <label for="name" class="sr-only">List Name</label>
+        <input v-model="name"
+               class="new-list__input"
+               type="text" id="name" placeholder="List Name">
+
+        <button type="submit" class="new-list__button">Create</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: null
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.new-list {
+  @apply grid place-items-center;
+
+  &__form {
+    @apply flex justify-center items-center;
+  }
+
+  &__container {
+    @apply shadow rounded;
+  }
+
+  &__input {
+    @apply bg-white px-3 py-2 rounded-l outline-none;
+  }
+
+  &__button {
+    @apply bg-gray-100 px-3 py-2 rounded-r transition duration-200 ease-in-out;
+
+    &:hover, &:focus {
+      @apply bg-green-100;
+    }
+  }
+}
+</style>
