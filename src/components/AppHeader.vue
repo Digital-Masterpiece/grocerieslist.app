b02f330457520dea8209b6c349793d79feee36d0
Nick Frost <nickfrost.us@gmail.com>
2021-09-05 10:13:48 -0400

    add fontawesome library for vue 3

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,17 +2,20 @@
   <header class="header">
     <div class="header__container">
       <router-link :to="{name: 'Lists'}">My Lists</router-link>
-      <router-link :to="{name: 'New'}">New List</router-link>
+      <router-link :to="{name: 'New'}">
+        <font-awesome-icon icon="plus-circle"/>
+        <span>New List</span>
+      </router-link>
     </div>
   </header>
 </template>
 
 <style lang="scss">
 .header {
-  @apply grid place-items-center;
+  @apply grid place-items-center bg-gl-blue py-6;
 
   &__container {
-    @apply flex justify-between w-full max-w-lg p-6 pb-0;
+    @apply flex justify-between w-full max-w-lg px-6 pb-0;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './assets/main.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPlusCircle)
 
 createApp(App)
   .use(store)
   .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
   .mount('#app')
