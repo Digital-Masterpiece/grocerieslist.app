a8f98f4d470c414fff40ceb1420e8f47105833fd
Nick Frost <nickfrost.us@gmail.com>
2021-09-05 18:22:28 -0400

    list deletion

--- src/views/Lists.vue
+++ src/views/Lists.vue
@@ -1,7 +1,16 @@
 <template>
   <div v-if="$store.state.lists.length" class="lists">
     <router-link v-for="list in $store.state.lists" :key="list.id" class="list"
-                 :to="{name: 'List', params: {id: list.id}}">{{ list.name }}
+                 :to="{name: 'List', params: {id: list.id}}">
+      <button :title="'Share your ' + list.name + ' list.'" class="list__icon--share">
+        <font-awesome-icon icon="share-alt-square"/>
+      </button>
+      <span>{{ list.name }}</span>
+      <button :title="'Delete your ' + list.name + ' list.'"
+              @click.prevent="deleteList(list.id)"
+              class="list__icon--delete">
+        <font-awesome-icon icon="times-circle"/>
+      </button>
     </router-link>
   </div>
   <div v-else class="no-lists">
@@ -12,15 +21,35 @@
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    deleteList (id) {
+      this.$store.dispatch('deleteList', id)
+    }
+  }
+}
 </script>
 
 <style lang="scss">
 .list {
-  @apply px-4 py-3 bg-white rounded border border-gl-lightblue w-full font-bold text-lg;
+  @apply flex justify-start items-center px-4 py-3 bg-white rounded border border-gl-lightblue w-full font-bold text-lg;
 
   &s {
     @apply grid w-full gap-4;
+  }
+
+  &__icon {
+    &--share {
+      @apply text-3xl mr-3;
+    }
+
+    &--delete {
+      @apply text-xl ml-auto;
+
+      &:hover, &:focus {
+        @apply text-red-700;
+      }
+    }
   }
 }
 
