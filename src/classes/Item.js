7ec2d4ffc4e8efb873d219a1504c30f18858e432
Nick Frost <nfrost.dev@gmail.com>
2021-11-11 11:06:21 -0500

    remove created key from items

--- src/classes/Item.js
+++ src/classes/Item.js
@@ -2,15 +2,11 @@ import { v4 as uuidv4 } from 'uuid'
 
 export default class Item {
   constructor (name, quantity) {
-    // Created and updated timestamps default to creation time.
-    const now = new Date().getTime()
-
     this.id = uuidv4()
     this.name = name
     this.quantity = quantity
     this.checked = false
-    this.created = now
-    this.updated = now
+    this.updated = new Date().getTime()
     this.deleted = null
   }
 }
